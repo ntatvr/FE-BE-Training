fee4d21bc05772129c0d7ca480524ffd6091d5cf
Anh Nguyen <ntatvr@gmail.com>
2018-09-22 22:35:51 +0700

    Update libs on local

---
+++ my-project/config.js
@@ -0,0 +1,18 @@
+var config = {};
+
+// Database Configuration
+// Localhost: 1
+// config.db_host = '127.0.0.1';
+// config.db_port = '3306';
+// config.db_user = 'root';
+// config.db_password = '';
+
+// Localhost: 2
+config.db_host = '127.0.0.1';
+config.db_port = '3306';
+config.db_user = 'root';
+config.db_password = '123456';
+
+
+module.exports = config
+
--- my-project/dao/core.server.dbconnection.js
+++ my-project/dao/core.server.dbconnection.js
@@ -1,12 +1,13 @@
 "user strict";
 
 const mysql = require('mysql');
+const config = require('../config');
 
 var connection = mysql.createPool({
-	host: '127.0.0.1',
- 	port: '3306',
-  	user: "root",
-  	password: ""
+	host: config.db_host,
+ 	port: config.db_port,
+  	user: config.db_user,
+  	password: config.db_password
 });
 
 module.exports = connection
