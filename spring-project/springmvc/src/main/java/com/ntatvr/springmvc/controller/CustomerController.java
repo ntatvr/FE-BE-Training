7c4bc92a0c5b6d4fc6732837611ab73a903ff1b5
baohoan90 <baohoan90@gmail.com>
2019-03-12 01:20:33 +0700

    don't need throw exception at service layer

--- spring-project/springmvc/src/main/java/com/ntatvr/springmvc/controller/CustomerController.java
+++ spring-project/springmvc/src/main/java/com/ntatvr/springmvc/controller/CustomerController.java
@@ -1,11 +1,15 @@
 package com.ntatvr.springmvc.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.ntatvr.springmvc.entity.Customer;
 import com.ntatvr.springmvc.exception.DataNotFoundException;
 import com.ntatvr.springmvc.service.CustomerService;
@@ -14,20 +18,23 @@ import com.ntatvr.springmvc.service.CustomerService;
 @RequestMapping("/customer")
 public class CustomerController {
 
-  @Autowired
-  private CustomerService customerService;
+	@Autowired
+	private CustomerService customerService;
 
-  /**
-   * Load customer by id
-   *
-   * @param id the id of customer
-   * @return the customer
-   * @throws DataNotFoundException
-   */
-  @GetMapping("/load/{id}")
-  @ResponseBody
-  public Customer loadCustomer(@PathVariable("id") Integer id) throws DataNotFoundException {
-    return customerService.getCustomer(id);
-  }
+	/**
+	 * Load customer by id
+	 *
+	 * @param id
+	 *            the id of customer
+	 * @return the customer
+	 * @throws DataNotFoundException
+	 */
+	@GetMapping("/load/{id}")
+	@ResponseBody
+	public ResponseEntity<Customer> loadCustomer(@PathVariable("id") Integer id) throws DataNotFoundException {
+		Customer customer = customerService.getCustomer(id);
+		return Optional.ofNullable(customer).map(user -> ResponseEntity.ok().body(user)) // 200 OK
+				.orElseGet(() -> ResponseEntity.notFound().build()); // 404 Not found
+	}
 
 }
--- spring-project/springmvc/src/main/java/com/ntatvr/springmvc/service/impl/CustomerServiceImpl.java
+++ spring-project/springmvc/src/main/java/com/ntatvr/springmvc/service/impl/CustomerServiceImpl.java
@@ -1,41 +1,41 @@
 package com.ntatvr.springmvc.service.impl;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.ntatvr.springmvc.entity.Customer;
 import com.ntatvr.springmvc.exception.DataNotFoundException;
 import com.ntatvr.springmvc.repository.CustomerRepository;
 import com.ntatvr.springmvc.service.CustomerService;
 
-
 @Service(value = "customerService")
 @Transactional
 public class CustomerServiceImpl implements CustomerService {
 
-  @Autowired
-  private CustomerRepository customerRepository;
+	@Autowired
+	private CustomerRepository customerRepository;
 
-  @Override
-  public List<Customer> getCustomers() {
-    return customerRepository.findAll();
-  }
+	@Override
+	public List<Customer> getCustomers() {
+		return customerRepository.findAll();
+	}
 
-  @Override
-  public void saveCustomer(Customer theCustomer) {
-    customerRepository.save(theCustomer);
-  }
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		customerRepository.save(theCustomer);
+	}
 
-  @Override
-  public Customer getCustomer(Integer theId) throws DataNotFoundException {
-    return customerRepository.findById(theId).orElseThrow(() -> new DataNotFoundException(theId));
-  }
+	@Override
+	public Customer getCustomer(Integer theId) throws DataNotFoundException {
+		return customerRepository.findById(theId).orElse(null);
+	}
 
-  @Override
-  public void deleteCustomer(Integer theId) throws DataNotFoundException {
-    customerRepository.deleteById(theId);
-  }
-
+	@Override
+	public void deleteCustomer(Integer theId) throws DataNotFoundException {
+		customerRepository.deleteById(theId);
+	}
 
 }
