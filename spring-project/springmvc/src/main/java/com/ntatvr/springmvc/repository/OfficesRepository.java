e3a9f700dcc2d855fc759d23204419f8ffefd69d
Anh Nguyen <ntatvr@gmail.com>
2019-03-20 22:11:26 +0700

    Create table offices

---
+++ spring-project/springmvc/src/main/java/com/ntatvr/springmvc/repository/OfficesRepository.java
@@ -0,0 +1,11 @@
+package com.ntatvr.springmvc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import com.ntatvr.springmvc.entity.Office;
+
+@Repository
+@Transactional(rollbackFor = Exception.class)
+public interface OfficesRepository extends JpaRepository<Office, Integer> {
+}
