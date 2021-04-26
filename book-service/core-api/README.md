fa97a94872560757831dd9b9e00ae2d49aa32d1e
ANT <anh.nguyen.tuan@pycogroup.com>
2021-04-26 17:51:26 +0700

    Delete book-service

--- book-service/core-api/README.md
+++
@@ -1,4 +0,0 @@
-# Prerequisite Condition
-- Install Docker
-
-## Run MongoDB Cluster in local using docker-compose
--- book-service/core-api/src/main/java/com/ntatvr/core/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ntatvr.core.repositories;
-
-import org.springframework.stereotype.Repository;
-
-import com.ntatvr.domain.entities.book.BookEntity;
-
-@Repository
-public interface BookRepository extends BaseRepository<BookEntity, String> {
-}
